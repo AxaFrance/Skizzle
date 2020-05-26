dfc2596d2dbbead253d8afca12b919bc8f2e2f18
Jérôme Boukorras <jerome.boukorras@axa.fr>
2020-05-26 17:14:28 +0200

    fix(Login): Logo for dark context

--- src/layouts/Login/Login.svelte
+++ src/layouts/Login/Login.svelte
@@ -10,6 +10,11 @@ const { ipcRenderer } = require('electron');
     src="./assets/logo-skizzle.svg"
     alt="Skizzle"
   />
+  <img
+    class="skz-login__logo skz-login__logo--dark"
+    src="./assets/logo-skizzle-dark-context.svg"
+    alt="Skizzle"
+  />
   <p class="skz-login__intro">
     Skizzle vous permet de regrouper les pull requests de vos équipes. <br/>Pour commencer <b>connectez vous</b> avec votre compte.
   </p>
