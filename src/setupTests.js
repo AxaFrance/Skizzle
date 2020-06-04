0c6b857a993537b039d99c8416951c38d41ddfe6
Debaerrdm <mathieu.debaerdemaeker@gmail.com>
2020-06-04 14:57:19 +0200

    fix(Test): mock function

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,11 +1,15 @@
 jest.mock('electron', () => ({
-  ipcRenderer: {
-    send: jest.fn(),
-    on: jest.fn(),
-  },
-  remote: {
-    app: {
-      getVersion: () => '1.0.0',
-    },
-  },
+	ipcRenderer: {
+		send: jest.fn(),
+		on: jest.fn(),
+	},
+	remote: {
+		app: {
+			getVersion: () => '1.0.0',
+		},
+		getCurrentWindow: () => ({
+			isMaximized: jest.fn(),
+			on: jest.fn(),
+		}),
+	},
 }));
