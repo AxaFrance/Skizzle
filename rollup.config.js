b28d4cb873233a36ca8017edd0fa1a7c42e0490b
Debaerrdm <mathieu.debaerdemaeker@gmail.com>
2020-06-04 17:04:50 +0200

    fix(Bundle): remove sourcemap of production app

--- rollup.config.js
+++ rollup.config.js
@@ -8,48 +8,48 @@ import { scss } from 'svelte-preprocess';
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/main.js',
-  output: {
-    sourcemap: true,
-    format: 'iife',
-    name: 'Skizzle',
-    file: 'public/build/bundle.js',
-  },
-  plugins: [
-    svelte({
-      dev: !production,
-      preprocess: [
-        scss({
-          output: true,
-          failOnError: true,
-        }),
-      ],
-      css: css => {
-        css.write('public/build/bundle.css');
-      },
-    }),
-    resolve({
-      browser: true,
-      dedupe: ['svelte'],
-    }),
-    commonjs(),
-    !production && serve(),
-    !production && livereload('public'),
-    production && terser(),
-  ],
-  watch: {
-    clearScreen: false,
-  },
+	input: 'src/main.js',
+	output: {
+		sourcemap: !production,
+		format: 'iife',
+		name: 'Skizzle',
+		file: 'public/build/bundle.js',
+	},
+	plugins: [
+		svelte({
+			dev: !production,
+			preprocess: [
+				scss({
+					output: true,
+					failOnError: true,
+				}),
+			],
+			css: css => {
+				css.write('public/build/bundle.css');
+			},
+		}),
+		resolve({
+			browser: true,
+			dedupe: ['svelte'],
+		}),
+		commonjs(),
+		!production && serve(),
+		!production && livereload('public'),
+		production && terser(),
+	],
+	watch: {
+		clearScreen: false,
+	},
 };
 
 function serve() {
-  let started = false;
+	let started = false;
 
-  return {
-    writeBundle() {
-      if (!started) {
-        started = true;
-      }
-    },
-  };
+	return {
+		writeBundle() {
+			if (!started) {
+				started = true;
+			}
+		},
+	};
 }
