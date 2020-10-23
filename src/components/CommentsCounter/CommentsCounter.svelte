6e51b509178c72d5a071a5592421554fb4ceaab7
Jérôme Boukorras <51956779+itupix@users.noreply.github.com>
2020-10-23 10:23:54 +0200

    fix(pull request): button for comments display (#86)
    Co-authored-by: itupix <jerome.boukorras@axa.fr>

--- src/components/CommentsCounter/CommentsCounter.svelte
+++ src/components/CommentsCounter/CommentsCounter.svelte
@@ -1,23 +1,30 @@
 <script>
-  export let comments = [];
-  export let hasResponse;
+	export let comments = [];
+	export let hasResponse;
+	export let action;
 
-  const isDisplayed = comments && comments.length > 0;
-  const isCommentResolved = ({ status }) =>
-    status && status !== 'active' && status !== 'pending';
-  const commentsResolved =
-    comments.filter(isCommentResolved).length === comments.length;
-  const classModifier = commentsResolved
-    ? 'skz-comments-counter--all-resolved'
-    : hasResponse
-    ? 'skz-comments-counter--responses'
-    : '';
+	const isDisplayed = comments && comments.length > 0;
+	const isCommentResolved = ({ status }) =>
+		status && status !== 'active' && status !== 'pending';
+	const commentsResolved =
+		comments.filter(isCommentResolved).length === comments.length;
+	const classModifier = commentsResolved
+		? 'skz-comments-counter--all-resolved'
+		: hasResponse
+		? 'skz-comments-counter--responses'
+		: '';
+
+	const onClick = e => {
+		e.preventDefault();
+		action();
+	};
 </script>
 
 <style src="./CommentsCounter.scss">
-
 </style>
 
 {#if isDisplayed}
-  <p class={`skz-comments-counter ${classModifier}`}>{comments.length}</p>
+	<p on:click={onClick} class={`skz-comments-counter ${classModifier}`}>
+		{comments.length}
+	</p>
 {/if}
