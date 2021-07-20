a303fdb2f7c7a9cd5d8fbedd6fda1c0094bfb5d9
DE BAERDEMAEKER Mathieu <mathieu.debaerdemaeker@axa.fr>
2021-07-20 11:53:33 +0200

    feat(mocks): adding mocks for development mode

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-//@ts-ignore
 import App from './App.svelte';
-import { worker } from "./mocks/browser";
+/*import { worker } from "./mocks/browser";
 
 if (!window.remote.isProduction) {
   worker.start();
-}
+}*/
 
 const app = new App({
 	target: document.body,
